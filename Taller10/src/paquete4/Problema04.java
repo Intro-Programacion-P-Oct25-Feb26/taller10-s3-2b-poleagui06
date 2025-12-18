dcd43a0131d12ae5043c11200aba56c763e340ec
poleagui06 <jeanpoleagui06@gmail.com>
2025-12-18 11:48:36 -0500

    solución Problema04

--- Taller10/src/paquete4/Problema04.java
+++ Taller10/src/paquete4/Problema04.java
@@ -5,10 +5,29 @@
  */
 package paquete4;
 
+import java.util.Scanner;
+
 /**
  *
  * @author reroes
  */
 public class Problema04 {
-    
+
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+
+        int[][] matrizA = new int[3][2];
+        int[][] matrizB = new int[3][2];
+
+        boolean bandera = true;
+
+        String reporte = "";
+
+        for (int i = 0; i < matrizA.length; i++) {
+            for (int j = 0; j < matrizA[i].length; j++) {
+                System.out.printf("Ingrese valor A[%d][%d]: ", i, j);
+                matrizA[i][j] = entrada.nextInt();
+            }
+        }
+    }
 }
