8c8124c13ad32035a75680d6c3a11e2cd502a49a
poleagui06 <jeanpoleagui06@gmail.com>
2025-12-18 11:23:33 -0500

    solución Problema01

--- Taller10/src/paquete1/Problema01.java
+++ Taller10/src/paquete1/Problema01.java
@@ -14,7 +14,55 @@ public class Problema01 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+
+        boolean[][] asistencia = {
+            {true, true, false, true, true},
+            {true, false, false, true, false},
+            {true, true, true, true, true},
+            {false, true, true, false, true}
+        };
+
+        String[] estudiantes = {"Ana", "Luis", "Carlos", "María"};
+
+        int[] diasAsistidos = new int[4];
+
+        int contador;
+        String reporte = "";
+        String mensaje = "";
+
+        for (int i = 0; i < asistencia.length; i++) {
+
+            contador = 0;
+
+            for (int j = 0; j < asistencia[i].length; j++) {
+
+                if (asistencia[i][j] == true) {
+                    contador = contador + 1;
+                }
+
+            }
+
+            diasAsistidos[i] = contador;
+        }
+
+        for (int i = 0; i < estudiantes.length; i++) {
+
+            if (diasAsistidos[i] == 5) {
+                mensaje = "Cumple asistencia completa";
+            } else {
+                mensaje = "No cumple asistencia completa";
+            }
+
+            reporte = String.format(
+                    "%sEstudiante: %s\n"
+                    + "Días asistidos: %d\n"
+                    + "Estado: %s\n\n",
+                    reporte,
+                    estudiantes[i],
+                    diasAsistidos[i],
+                    mensaje);
+        }
+
+        System.out.printf("%s", reporte);
     }
-    
 }
