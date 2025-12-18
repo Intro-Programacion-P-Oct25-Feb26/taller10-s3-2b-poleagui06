7157590fde5d658cffc6aa2744bf643057612158
poleagui06 <jeanpoleagui06@gmail.com>
2025-12-18 11:38:08 -0500

    solución Problema03

--- Taller10/src/paquete3/Problema03.java
+++ Taller10/src/paquete3/Problema03.java
@@ -10,5 +10,46 @@ package paquete3;
  * @author reroes
  */
 public class Problema03 {
-    
+
+    public static void main(String[] args) {
+        boolean[][] sensores = {
+            {true, true, true, true, true, true},
+            {true, false, true, false, true, false},
+            {false, false, false, false, false, false},
+            {true, true, false, true, true, false}
+        };
+
+        int correctos = 0;
+        boolean fallos;
+        String reporte = "";
+
+        for (int i = 0; i < sensores.length; i++) {
+
+            fallos = false;
+
+            for (int j = 0; j < sensores[i].length; j++) {
+
+                if (sensores[i][j] == false) {
+                    fallos = true;
+                }
+
+            }
+
+            if (fallos == false) {
+                correctos = correctos + 1;
+            } else {
+                reporte = String.format(
+                        "%sLa zona %d presenta al menos un fallo\n",
+                        reporte,
+                        i + 1);
+            }
+        }
+
+        reporte = String.format(
+                "%s\nTotal de zonas que funcionan sin fallos: %d\n",
+                reporte,
+                correctos);
+
+        System.out.printf("%s", reporte);
+    }
 }
